ae9056c82958212b7df665ee5ab4b0db8c4772bc
Nick Confrey <nconfrey@charmander.cs.uchicago.edu>
2015-11-23 23:31:50 -0600

    still not working

--- final-project/src/plane.hxx
+++ final-project/src/plane.hxx
@@ -7,6 +7,7 @@
 #define _PLANE_HXX_
 
 #include "cs237.hxx"
+#include "camera.hxx"
 
 class Plane {
 	public:
@@ -21,9 +22,9 @@ class Plane {
 		Plane();
 		Plane(float a, float b, float c, float d);
 
-		Plane ** extractPlanes(cs237::mat4x4f projTransform);
-		void NormalizePlane(Plane * plane);
-		int ClassifyPoint(const cs237::vec3d & pt);
+		Plane ** extractPlanes(Camera c, cs237::mat4x4f projTransform);
+		void NormalizePlane();
+		int ClassifyPoint(const cs237::vec3d & pt, cs237::mat4x4f ModelViewMatrix);
 
 		float a;
 		float b;
