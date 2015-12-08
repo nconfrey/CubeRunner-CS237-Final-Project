8561ed672a2418e200d80974565e74026e977eba
Nick Confrey <nickconfrey@earthlink.net>
2015-12-08 11:09:35 -0600

    SCORING WORKING DYNAMICALLY OMG

--- final-project/src/view.cxx
+++ final-project/src/view.cxx
@@ -524,7 +524,7 @@ void View::Render ()
     //DRAW SKYBOX
     skybox->Render(this->projectionMat, this->modelViewMat, this->Camera().position(), 
       true, cs237::color3f(0.0, 0.0, 0.0), 0.0035f);
-    text->Render(cs237::vec2f(this->_fbWid, this->_fbHt), 9999);
+    text->Render(cs237::vec2f(this->_fbWid, this->_fbHt), this->score);
 
     //DRAW GROUND
     r->Enable(this->projectionMat, this->sun);
